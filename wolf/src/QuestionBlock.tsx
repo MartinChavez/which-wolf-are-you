91a8ce524d5a7cb7296631d99aa712152148f9c7
Martin Chavez <info@martinchavezaguilar.com>
2020-04-27 06:55:35 -0700

    Add questions to QuestionBlock

--- wolf/src/QuestionBlock.tsx
+++ wolf/src/QuestionBlock.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./QuestionBlock.css";
 
+interface QuestionProps {
+  questionNumber: number;
+}
+
+function Question(props: QuestionProps) {
+  return <p>Question {props.questionNumber}</p>;
+}
+
 function QuestionBlock() {
   return (
     <>
-      <p className="question-block"> Question block</p>
+      <h4 className="question-block"> Question block</h4>
+      <Question questionNumber={1}></Question>
+      <Question questionNumber={2}></Question>
+      <Question questionNumber={3}></Question>
+      <Question questionNumber={4}></Question>
     </>
   );
 }
