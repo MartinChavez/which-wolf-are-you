9a3271c9163ce74e4d22915db46e2900c0d0f3cf
Martin Chavez <info@martinchavezaguilar.com>
2020-05-18 05:29:02 -0700

    Add Reducer to handle b utton coloring

--- wolf/src/QuestionBlock.tsx
+++ wolf/src/QuestionBlock.tsx
@@ -1,10 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect, useReducer } from "react";
 import "./QuestionBlock.css";
 import { ThemeContext } from "./ThemeContext";
 import { IQuestion, IAnswer } from "./Questions";
 
 type AnswerProps = {
+  answerId: number;
   answer: string;
+  state: QuestionBlockState;
+  dispatch: React.Dispatch<QuestionBlockAction>;
 };
 
 export type QuestionBlockProps = {
@@ -14,15 +17,71 @@ export type QuestionBlockProps = {
 
 function Answer(props: AnswerProps) {
   const theme = useContext(ThemeContext);
-  return <p style={{ color: theme.textColor }}>Answer {props.answer}</p>;
+  const [backgroundColor, setBackgroundColor] = useState("white");
+
+  const buttonClick = () => {
+    props.dispatch({ type: "questionSelected", answerId: props.answerId });
+  };
+
+  useEffect(() => {
+    if (props.state.questionSelectedForBlock === props.answerId) {
+      setBackgroundColor("grey");
+    } else {
+      setBackgroundColor("white");
+    }
+  }, [props.answerId, props.state.questionSelectedForBlock]);
+
+  return (
+    <button
+      onClick={buttonClick}
+      style={{
+        color: theme.textColor,
+        backgroundColor: backgroundColor,
+        display: "block",
+      }}
+    >
+      {props.answer}
+    </button>
+  );
+}
+
+type QuestionBlockState = {
+  questionSelectedForBlock: number;
+};
+
+type QuestionBlockAction = {
+  type: string;
+  answerId: number;
+};
+
+const initialState: QuestionBlockState = {
+  questionSelectedForBlock: 0,
+};
+
+function reducer(state: QuestionBlockState, action: QuestionBlockAction) {
+  switch (action.type) {
+    case "questionSelected":
+      return { questionSelectedForBlock: action.answerId };
+    default:
+      throw new Error();
+  }
 }
 
 function QuestionBlock(props: QuestionBlockProps) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  //const [questionSelectedForBlock, setQuestionSelectedForBlock] = useState(0);
+
   return (
     <>
       <h4 className="question-block"> {props.question.question}</h4>
       {props.answers.map((ans) => (
-        <Answer key={ans.id} answer={ans.answer}></Answer>
+        <Answer
+          key={ans.id}
+          answer={ans.answer}
+          answerId={ans.id}
+          state={state}
+          dispatch={dispatch}
+        ></Answer>
       ))}
     </>
   );
