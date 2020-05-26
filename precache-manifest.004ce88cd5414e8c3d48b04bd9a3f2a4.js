4836717d9812715c79081b22cab44b9fa43aef07
Martin Chavez <info@martinchavezaguilar.com>
2020-05-26 07:15:28 -0700

    Updates

---
+++ precache-manifest.004ce88cd5414e8c3d48b04bd9a3f2a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9394dab8c5df8f60d65c8e927718950",
+    "url": "/which-wolf-are-you/index.html"
+  },
+  {
+    "revision": "1ea4f9c7c8eb01328043",
+    "url": "/which-wolf-are-you/static/css/main.601835ae.chunk.css"
+  },
+  {
+    "revision": "febf010fbcc83cefac77",
+    "url": "/which-wolf-are-you/static/js/2.32460686.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/which-wolf-are-you/static/js/2.32460686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea4f9c7c8eb01328043",
+    "url": "/which-wolf-are-you/static/js/main.b3b7c931.chunk.js"
+  },
+  {
+    "revision": "349b0b67cd6fb0e98a48",
+    "url": "/which-wolf-are-you/static/js/runtime-main.8cf87baf.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/which-wolf-are-you/precache-manifest.004ce88cd5414e8c3d48b04bd9a3f2a4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/which-wolf-are-you/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
