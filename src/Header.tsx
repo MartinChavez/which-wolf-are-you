7620305a862f8fe6fde386cd7d503dedffa72600
Martin Chavez <hello@martinchavez.dev>
2020-06-30 06:08:18 -0700

    Only show sun or moon per state.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { useState, Dispatch, SetStateAction } from "react";
 import { themes, ThemeInfo } from "./ThemeContext";
 import "./Header.css";
 
@@ -8,29 +8,38 @@ type HeaderProps = {
 };
 
 function Header(props: HeaderProps) {
+  const [showSun, setShowSun] = useState(true);
+
   let themButtonClick = (
     event: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
   ) => {
     if (event.currentTarget.name === "Day") {
-      props.setTheme(themes.day);
-    } else if (event.currentTarget.name === "Night") {
       props.setTheme(themes.night);
+      setShowSun(false);
+    } else if (event.currentTarget.name === "Night") {
+      props.setTheme(themes.day);
+      setShowSun(true);
     }
   };
 
   return (
     <div className="header">
       <h4>Header</h4>
-      <button name="Day" onClick={themButtonClick}>
-        <span role="img" aria-label="Day">
-          ☀️
-        </span>
-      </button>
-      <button name="Night" onClick={themButtonClick}>
-        <span role="img" aria-label="Day">
-          🌘
-        </span>
-      </button>
+      {showSun && (
+        <button name="Day" onClick={themButtonClick}>
+          <span role="img" aria-label="Day">
+            ☀️
+          </span>
+        </button>
+      )}
+      {!showSun && (
+        <button name="Night" onClick={themButtonClick}>
+          <span role="img" aria-label="Day">
+            🌘
+          </span>
+        </button>
+      )}
+
       <button onClick={props.resetButtonClick}>Reset</button>
     </div>
   );
