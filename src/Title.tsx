b12d5655e1951a9f3c8478c40574edf593c45e5f
Martin Chavez <hello@martinchavez.dev>
2020-07-07 08:11:19 -0700

    Add effect to change title color.

--- src/Title.tsx
+++ src/Title.tsx
@@ -1,18 +1,20 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ThemeContext } from "./ThemeContext";
-import titleWhite from "./Title-White.svg";
-import titleDark from "./Title.svg";
+import titleWhite from "./Title White.svg";
+import titleDark from "./Title Black.svg";
 
 function Title() {
   const theme = useContext(ThemeContext);
 
-  const [selectedTitle] = useState(
-    theme.name === "day" ? titleDark : titleWhite
-  );
+  const [selectedTitle, setSelectedTitle] = useState(titleDark);
+
+  useEffect(() => {
+    setSelectedTitle(theme.name === "day" ? titleDark : titleWhite);
+  }, [theme.name]);
 
   return (
     <div className="title">
-      <img src={selectedTitle} alt="Logo" height="200px" width="auto" />
+      <img src={selectedTitle} alt="Logo" height="65px" width="auto" />
     </div>
   );
 }
